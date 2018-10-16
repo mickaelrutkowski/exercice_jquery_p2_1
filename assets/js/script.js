c44e87b40cedf471f44fb6cf710388f4f667ab5b
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-10-16 22:39:40 +0200

    exercice 1 partie 3 JQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
+
 $(function() {
-    $('#clickMe').click(function() {
-alert('test');
+  var count = 0;
+  $("#update").click(function() {
+    count++;
+    $("#counter").html("nombre de clics: "+count);
   });
 });
+// $(function() {
+//   var count = 0;
+//   $("#update1").click(function() {
+//     count;
+//     $("#counter").html("nombre de clics: "-count);
+//   });
+// });
